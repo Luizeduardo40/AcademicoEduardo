422867ed317beda5c1966a6680641b8447df8f60
Luiz Eduardo <luiz.eduardo40@gmail.com>
2024-09-11 22:34:08 -0300

    Classe tecnico criada e Menu atualizado

--- src/br/edu/ifs/academico/Menu.java
+++ src/br/edu/ifs/academico/Menu.java
@@ -20,6 +20,8 @@ public class Menu {
 		System.out.println("8. Imprimir Lista Pedagogos");
         System.out.println("9. Cadastrar Psicologo");
 		System.out.println("10. Imprimir Lista Psicologos");
+        System.out.println("11. Cadastrar Tecnico");
+		System.out.println("12. Imprimir Lista Tecnicos");
 		System.out.println("0. Sair");
 	}
 
---
+++ src/br/edu/ifs/academico/Tecnico.java
@@ -0,0 +1,36 @@
+package br.edu.ifs.academico;
+
+//Classe Tecnico extends Pessoa
+public class Tecnico extends Pessoa {
+
+    //Iniciando atributos
+    private String profissao;
+    private String laboratorio;
+    private int sala;
+
+    //Encapsulando atributos
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+
+    public int getSala() {
+        return sala;
+    }
+
+    public void setSala(int sala) {
+        this.sala = sala;
+    }
+
+}
