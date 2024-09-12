3f5e7b500cf4be12ae89dadc340d8e9c7505df87
Luiz Eduardo <luiz.eduardo40@gmail.com>
2024-09-11 22:13:36 -0300

    Criacao da classe Menu

---
+++ src/br/edu/ifs/academico/Menu.java
@@ -0,0 +1,20 @@
+package br.edu.ifs.academico;
+
+//Classe Menu
+public class Menu {
+
+    //Exibe o menu na tela
+    public void imprimirMenu() {
+		
+		System.out.println("Sistema Acadamico IFS");
+		System.out.println("        Menu");
+		System.out.println();
+		System.out.println("Selecione uma opcao:");
+		System.out.println("1. Cadastrar Aluno");
+		System.out.println("2. Imprime Lista Alunos");
+		System.out.println("3. Cadastrar Professor");
+		System.out.println("4. Imprimir Lista Professores");
+		System.out.println("0. Sair");
+	}
+
+}
