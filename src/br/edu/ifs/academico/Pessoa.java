50460db3bbbb073d40778e3ee74d10f78dad4ad4
Luiz Eduardo <luiz.eduardo40@gmail.com>
2024-09-11 22:02:04 -0300

    Criacao da classe base Pessoa

---
+++ src/br/edu/ifs/academico/Pessoa.java
@@ -0,0 +1,42 @@
+package br.edu.ifs.academico;
+
+public class Pessoa {
+
+    private String nome;
+    private String dataNascimento;
+    private String localNascimento;
+    private char sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getLocalNascimento() {
+        return localNascimento;
+    }
+
+    public void setLocalNascimento(String localNascimento) {
+        this.localNascimento = localNascimento;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+}
